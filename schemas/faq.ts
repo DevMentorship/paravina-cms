6a3dba626c93383ae19671eeb837462008868451
Aleksey Likhtinov <91214571+Grec88@users.noreply.github.com>
2023-04-24 22:34:22 +0300

    FAQ

---
+++ schemas/faq.ts
@@ -0,0 +1,25 @@
+import { defineField, defineType, defineArrayMember } from "sanity";
+
+export default defineType({
+    title: "Faq",
+    type: "document",
+    name: "faq",
+    fields: [
+        defineField({
+            title: "FaqItems",
+            type: "array",
+            name: "faqItems",
+            of: [
+                defineArrayMember({
+                    type: "object",
+                    name: "inline",
+                    fields: [
+                        defineField({ type: "string", name: "title" }),
+                        defineField({ type: "string", name: "content" }),
+                    ],
+                }),
+            ],
+
+        })
+    ]
+})
