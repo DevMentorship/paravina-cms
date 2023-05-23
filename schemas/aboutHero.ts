4dedd1761bee31939fd432e76c1589b0219bc2b1
Anton Potapov <panton160786@gmail.com>
2023-05-23 12:24:29 +0930

    About Hero schema has created

---
+++ schemas/aboutHero.ts
@@ -0,0 +1,27 @@
+export default {
+    name: 'aboutHero',
+    type: 'document',
+    title: 'About Hero Page',
+    fields: [
+        {
+            name: 'main_title',
+            type: 'string',
+            title: 'First Title'
+        },
+        {
+            name: 'second_title',
+            type: 'string',
+            title: 'Second Title'
+        },
+        {
+            name: 'descr',
+            type: 'text',
+            title: 'Text'
+        },
+        {
+            name: 'image',
+            type: 'image',
+            title: 'Image'
+        },
+    ]
+}
