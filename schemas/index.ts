10c37cea450de2dfed3be19e9370b0488cdb0b8b
kozurasergj <kozura.sergj33@gmail.com>
2023-06-03 16:17:38 +0300

    add schem price

--- schemas/index.ts
+++ schemas/index.ts
@@ -3,5 +3,6 @@ import category from './category'
 import post from './post'
 import author from './author'
 import faq from './faq'
+import price from './price'
 
-export const schemaTypes = [post, author, category, blockContent, faq]
+export const schemaTypes = [post, author, category, blockContent, faq, price]
